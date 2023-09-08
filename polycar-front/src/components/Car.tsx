2c5dc82f09c22d109b14a925434d927c0b66416a
Claquettes <math@claq.fr>
2023-09-08 12:05:32 +0200

    better cards

--- polycar-front/src/components/Car.tsx
+++ polycar-front/src/components/Car.tsx
@@ -8,27 +8,29 @@ function Car(props: CarProps) {
   const { marque, modele, annee, prixDeBase, image } = props;
   return (
     <div className="Car">
-      <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src={image} />
-        <Card.Body>
-          <div id="CarBody">
-            <Card.Title>
-              <p id="CarMarque">{marque}</p>
-            </Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              <p id="CarModele">{modele}</p>
-            </Card.Subtitle>
-            <Card.Text>
-              <div id="CarAnnee">
-                <p>Commercialisation: {annee}</p>
-              </div>
-              <div id="CarPrix">
-                <p>Prix de base: {prixDeBase}€</p>
-              </div>
-            </Card.Text>
-            <Button variant="primary">See more</Button>
-          </div>
-        </Card.Body>
+      <Card>
+        <div id="CarContent">
+          <Card.Img variant="top" src={image} />
+          <Card.Body>
+            <div id="CarBody">
+              <Card.Title>
+                <p id="CarMarque">{marque}</p>
+              </Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                <p id="CarModele">{modele}</p>
+              </Card.Subtitle>
+              <Card.Text>
+                <div id="CarAnnee">
+                  <p>Commercialisation: {annee}</p>
+                </div>
+                <div id="CarPrix">
+                  <p>Prix de base: {prixDeBase}€</p>
+                </div>
+              </Card.Text>
+              <Button variant="primary">See more</Button>
+            </div>
+          </Card.Body>
+        </div>
       </Card>
     </div>
   );
