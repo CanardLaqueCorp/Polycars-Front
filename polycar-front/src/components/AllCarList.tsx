f85c514d7a8de8a394756c26315d2f4ea16a967c
Claquettes <math@claq.fr>
2023-09-08 09:53:38 +0200

    cars.length

--- polycar-front/src/components/AllCarList.tsx
+++ polycar-front/src/components/AllCarList.tsx
@@ -4,17 +4,20 @@ import * as CarData from "../data/fakeCarData.json";
 function AllCarList() {
   const cars = CarData.cars;
   return (
-    <div className="AllCarList">
-      {cars.map((car) => (
-        <Car
-          marque={car.marque}
-          modele={car.modele}
-          annee={car.annee}
-          prixDeBase={car.prixDeBase}
-          image={car.image}
-        />
-      ))}
-    </div>
+    <>
+      <p>We have currently {cars.length} cars in our database</p>
+      <div className="AllCarList">
+        {cars.map((car) => (
+          <Car
+            marque={car.marque}
+            modele={car.modele}
+            annee={car.annee}
+            prixDeBase={car.prixDeBase}
+            image={car.image}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
