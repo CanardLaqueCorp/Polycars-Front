e28af1fe021901686c84c01920fdada541cea4e3
Claquettes <math@claq.fr>
2023-09-09 10:37:28 +0200

    better and cleaner css for car and carlist

--- polycar-front/src/components/Car.tsx
+++ polycar-front/src/components/Car.tsx
@@ -1,33 +1,24 @@
 import CarProps from "../interface/CarProps";
 import "../styles/car.scss";
 
-function Car(props: CarProps) {
-  const { marque, modele, annee, prixDeBase, image } = props;
+function Car({ marque, modele, annee, prixDeBase, image }: CarProps) {
   return (
-    <div className="Car">
-      <div className="card">
-        <div id="CarContent">
-          <img className="card-img-top" src={image} alt="Car image" />
-          <div className="card-body">
-            <div id="CarBody">
-              <h5 className="card-title">
-                <p id="CarMarque">{marque}</p>
-              </h5>
-              <h6 className="card-subtitle mb-2 text-muted">
-                <p id="CarModele">{modele}</p>
-              </h6>
-              <div className="card-text">
-                <div id="CarAnnee">
-                  <p>Commercialisation: {annee}</p>
-                </div>
-                <div id="CarPrix">
-                  <p>Prix de base: {prixDeBase}€</p>
-                </div>
-              </div>
-              <button className="btn btn-primary">See more</button>
-            </div>
-          </div>
+    <div className="Car card">
+      <div className="carHeader">
+        <img className="card-img-top" src={image} alt="Car image" />
+        <h5 className="card-title">
+          <p className="CarMarque">{marque}</p>
+          <p className="CarModele">{modele}</p>
+        </h5>
+      </div>
+      <div className="card-body">
+        <div className="CarAnnee">
+          <p>Commercialisation: {annee}</p>
         </div>
+        <div className="CarPrix">
+          <p>Prix de base: {prixDeBase}€</p>
+        </div>
+        <button className="btn btn-primary">See more</button>
       </div>
     </div>
   );
