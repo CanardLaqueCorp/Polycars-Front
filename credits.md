5ba3431a809634caf29c282e1e900e13af447b53
Claquettes <math@claq.fr>
2023-09-09 18:30:05 +0200

    started to create a front page with random car pictures

---
+++ credits.md
@@ -0,0 +1,8 @@
+credits photos:
+
+Photo by <a href="https://unsplash.com/@steven1302?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sven D</a> on <a href="https://unsplash.com/photos/a4S6KUuLeoM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@jamie452?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jamie Street</a> on <a href="https://unsplash.com/photos/JtP_Dqtz6D8?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+
+Photo by <a href="https://unsplash.com/@scottwebb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Scott Webb</a> on <a href="https://unsplash.com/photos/6nzYYtJtVDk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
--- polycar-front/src/components/FrontPage.tsx
+++ polycar-front/src/components/FrontPage.tsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 import "../styles/frontpage.scss";
 
+const carImageList = [
+    "https://claq.fr/host/random-car.jpg",
+    "https://claq.fr/host/random-car2.jpg",
+    "https://claq.fr/host/random-car3.jpg"
+];
+
+function changeBackground() {
+    const carImage = document.querySelector(".HomePageBackground") as HTMLDivElement;
+    carImage.style.backgroundImage = `url(${carImageList[Math.floor(Math.random() * carImageList.length)]})`;
+}
+
 function HomePage() {
+    const randomIndex = Math.floor(Math.random() * carImageList.length);
+    const randomImage = carImageList[randomIndex];
     return (
         <div className="HomePage">
+            <div className="HomePageBackground" style={{ backgroundImage: `url(${randomImage})` }}></div>
             <div className="HomePageHeader">
                 <h1 className="HomePageTitle">Welcome to PolyCars</h1>
                 <p className="HomePageSubtitle">
@@ -14,16 +28,17 @@ function HomePage() {
                     We have more than 7 cars in our database And a home-made quizz to help you find the car that suits you the best!
                 </p>
                 <div className="HomePageButtonContainer">
-                <Link to="/cars" className="HomePageButton">
-                    See Our Cars
-                </Link>
-                <Link to="/quizz" className="HomePageButton">
-                    Do the Quizz in miniutes!
-                </Link>
+                    <Link to="/cars" className="HomePageButton">
+                        See Our Cars
+                    </Link>
+                    <Link to="/quizz" className="HomePageButton">
+                        Do the Quizz in miniutes!
+                    </Link>
                 </div>
             </div>
         </div>
     );
 }
+setInterval(changeBackground, 5000);
 
 export default HomePage;
