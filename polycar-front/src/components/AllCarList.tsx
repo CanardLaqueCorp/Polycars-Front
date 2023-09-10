dca87f36fea447ab82570b191d6d4686897c7f3c
Claquettes <math@claq.fr>
2023-09-10 10:58:58 +0200

    text in header now modal + reset button

--- polycar-front/src/components/AllCarList.tsx
+++ polycar-front/src/components/AllCarList.tsx
@@ -7,48 +7,56 @@ function AllCarList() {
   const [filterValue, setFilterValue] = useState("");
   const cars = CarData.cars.filter((car: {[key: string]: string}) => 
     car[filterType].toLowerCase().includes(filterValue.toLowerCase())
-);
+  );
 
-const handleFilterTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => { //on doit préciser le type de l'event, sinon il ne sait pas ce qu'il y a dedans
-  setFilterType(e.target.value);
-};
+  const handleFilterTypeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setFilterType(e.target.value);
+  };
 
-const handleFilterValueChange = (e: React.ChangeEvent<HTMLInputElement>) => { //idem
-  setFilterValue(e.target.value);
-};
-return (
-  <div className="AllCarListComponent">
-    <div className="AllCarListHeader">
-      <h1>Car List:</h1>
-      <p>We have currently {cars.length} cars in our database</p>
-      <div>
-        <select value={filterType} onChange={handleFilterTypeChange}>
-          <option value="marque">Marque</option>
-          <option value="prixDeBase">Price</option>
-          <option value="modele">Modele</option>
-        </select>
-        <input
-          type="text"
-          placeholder={`Enter your ${filterType}`}
-          value={filterValue}
-          onChange={handleFilterValueChange}
-        />
+  const handleFilterValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFilterValue(e.target.value);
+  };
+
+  return (
+    <div className="AllCarListComponent">
+      <div className="AllCarListHeader">
+        <h1>Car List:</h1>
+        <p>
+          {filterValue === "" ? ( //le ? est un if et le : est un else, comme en python
+            `We have ${cars.length} cars in our database`
+          ) : (
+            `${cars.length} results for your search`
+          )}
+        </p>
+        <div>
+          <select value={filterType} onChange={handleFilterTypeChange}>
+            <option value="marque">Marque</option>
+            <option value="prixDeBase">Price</option>
+            <option value="modele">Modele</option>
+          </select>
+          <input
+            type="text"
+            placeholder={`Enter your ${filterType}`}
+            value={filterValue}
+            onChange={handleFilterValueChange}
+          />
+          <button onClick={() => setFilterValue("")}>Reset</button>
+        </div>
+      </div>
+      <div className="AllCarList">
+        {cars.map((car) => (
+          <Car
+            key={car.id}
+            id={car.id}
+            marque={car.marque}
+            modele={car.modele}
+            annee={car.annee}
+            prixDeBase={car.prixDeBase}
+            image={car.image}
+          />
+        ))}
       </div>
     </div>
-    <div className="AllCarList">
-      {cars.map((car) => (
-        <Car
-          key={car.id}
-          id={car.id}
-          marque={car.marque}
-          modele={car.modele}
-          annee={car.annee}
-          prixDeBase={car.prixDeBase}
-          image={car.image}
-        />
-      ))}
-    </div>
-  </div>
   );
 }
 
