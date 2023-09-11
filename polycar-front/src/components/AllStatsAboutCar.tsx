013fcf48d346b7f8f577a8b7d2417edbc9201223
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-11 19:32:07 +0200

    all stats for car but there is a problem in the compilation

--- polycar-front/src/components/AllStatsAboutCar.tsx
+++ polycar-front/src/components/AllStatsAboutCar.tsx
@@ -1,25 +1,95 @@
 import DisplayStats from "./displayStats";
-import * as dataOfTheCar from "../data/car-first-data.json";
-
+import * as dataOfTheCar from "../data/car-data-updated.json";
 
 function AllStatsAboutCar() {
-  //on récupère l'id de la voiture dans l'url
+  // Get the car ID from the URL
   const idOfCar: string = window.location.href.split("/")[4];
-  //on récupère les données de la voiture en utilisant getAllCarData
-  console.log("coucou");
-  //on recupère les données de toutes la voiture correspondant à l'id
-  const data=dataOfTheCar.result.filter((car) => car.id === parseInt(idOfCar));
-  console.log(data);
-  //on récupère les données de la voiture
-  const carData = data[0];
-  const marque: string = carData.brand;
-  const model: string = carData.model;
-  /// ...
+
+  // Get the car data by filtering the JSON based on the ID
+  const carData = dataOfTheCar.result.find((car) => car.id === parseInt(idOfCar));
+
+  // Destructure the carData object to extract its properties
+  const {
+    id,
+    brand,
+    model,
+    carType,
+    carTypeId,
+    priceNew,
+    priceUsed,
+    cylinder,
+    transmissionType,
+    transmissionTypeId,
+    transmissionTypeCode,
+    transmission,
+    gears,
+    driveSystem,
+    driveSystemId,
+    driveSystemCode,
+    fuel,
+    fuelDetail,
+    fuelId,
+    fuelCode,
+    maxBioFuel,
+    hasStartAndStop,
+    cityFuel,
+    cityCarbon,
+    highwayFuel,
+    highwayCarbon,
+    combinedFuel,
+    combinedCarbon,
+    hasGuzzler,
+    annualFuelCost,
+    spendOnFiveYears,
+    feRating,
+    ghgRating,
+    smogRating,
+    ecoScore,
+  } = carData;
 
   return (
     <div>
       <h1>Stats about car</h1>
-      <DisplayStats idOfCar={marque} />
+      {/* Pass the extracted variables as props to DisplayStats */}
+      <DisplayStats
+        id={id}
+        brand={brand}
+        model={model}
+        carType={carType}
+        carTypeId={carTypeId}
+        priceNew={priceNew}
+        priceUsed={priceUsed}
+        cylinder={cylinder}
+        transmissionType={transmissionType}
+        transmissionTypeId={transmissionTypeId}
+        transmissionTypeCode={transmissionTypeCode}
+        transmission={transmission}
+        gears={gears}
+        driveSystem={driveSystem}
+        driveSystemId={driveSystemId}
+        driveSystemCode={driveSystemCode}
+        fuel={fuel}
+        fuelId={fuelId}
+        fuelCode={fuelCode}
+        fuelDetail={fuelDetail}
+        maxBioFuel={maxBioFuel}
+        hasStartAndStop={hasStartAndStop}
+        cityFuel={cityFuel}
+        cityCarbon={cityCarbon}
+        highwayFuel={highwayFuel}
+        highwayCarbon={highwayCarbon}
+        combinedFuel={combinedFuel}
+        combinedCarbon={combinedCarbon}
+        hasGuzzler={hasGuzzler}
+        annualFuelCost={annualFuelCost}
+        spendOnFiveYears={spendOnFiveYears}
+        feRating={feRating}
+        ghgRating={ghgRating}
+        smogRating={smogRating}
+        ecoScore={ecoScore}
+
+        
+      />
     </div>
   );
 }
