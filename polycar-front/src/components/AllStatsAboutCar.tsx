8a0107e13e5449c97e78d553f5f1c431eaab4dc0
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-11 19:07:32 +0200

    frist version for first data

--- polycar-front/src/components/AllStatsAboutCar.tsx
+++ polycar-front/src/components/AllStatsAboutCar.tsx
@@ -1,19 +1,27 @@
 import DisplayStats from "./displayStats";
+import * as dataOfTheCar from "../data/car-first-data.json";
 
-function getAllCarData() {
-  //on recupère les données de toutes la voiture correspondant à l'id
-}
 
 function AllStatsAboutCar() {
   //on récupère l'id de la voiture dans l'url
   const idOfCar: string = window.location.href.split("/")[4];
+  //on récupère les données de la voiture en utilisant getAllCarData
+  console.log("coucou");
+  //on recupère les données de toutes la voiture correspondant à l'id
+  const data=dataOfTheCar.result.filter((car) => car.id === parseInt(idOfCar));
+  console.log(data);
+  //on récupère les données de la voiture
+  const carData = data[0];
+  const marque: string = carData.brand;
+  const model: string = carData.model;
+  /// ...
+
   return (
     <div>
       <h1>Stats about car</h1>
-      <DisplayStats idOfCar={idOfCar} />
+      <DisplayStats idOfCar={marque} />
     </div>
   );
 }
 
-getAllCarData();
 export default AllStatsAboutCar;
