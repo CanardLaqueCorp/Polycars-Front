8e720c91a3188bb047b6c4fa70a7210a2446f8a5
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-11 21:41:38 +0200

    fueldetail remove finish

--- polycar-front/src/components/displayStats.tsx
+++ polycar-front/src/components/displayStats.tsx
@@ -33,67 +33,41 @@ interface Props {
   ghgRating: number;
   smogRating: number;
   ecoScore: number;
-}
-
-function DisplayStats({
-  id,
-  model,
-  carType,
-  priceNew,
-  priceUsed,
-  cylinder,
-  transmissionType,
-  gears,
-  driveSystem,
-  fuel,
-  maxBioFuel,
-  hasStartAndStop,
-  cityFuel,
-  cityCarbon,
-  highwayFuel,
-  highwayCarbon,
-  combinedFuel,
-  combinedCarbon,
-  hasGuzzler,
-  annualFuelCost,
-  spendOnFiveYears,
-  feRating,
-  ghgRating,
-  smogRating,
-  ecoScore,
-}: Props) {
-  return (
-    <div>
-      <h1>Stats about car</h1>
-      <ul>
-        <li>id: {id}</li>
-        <li>model: {model}</li>
-        <li>carType: {carType}</li>
-        <li>priceNew: {priceNew}</li>
-        <li>priceUsed: {priceUsed}</li>
-        <li>cylinder: {cylinder}</li>
-        <li>transmissionType: {transmissionType}</li>
-        <li>gears: {gears}</li>
-        <li>driveSystem: {driveSystem}</li>
-        <li>fuel: {fuel}</li>
-        <li>maxBioFuel: {maxBioFuel}</li>
-        <li>hasStartAndStop: {hasStartAndStop}</li>
-        <li>cityFuel: {cityFuel}</li>
-        <li>cityCarbon: {cityCarbon}</li>
-        <li>highwayFuel: {highwayFuel}</li>
-        <li>highwayCarbon: {highwayCarbon}</li>
-        <li>combinedFuel: {combinedFuel}</li>
-        <li>combinedCarbon: {combinedCarbon}</li>
-        <li>hasGuzzler: {hasGuzzler}</li>
-        <li>annualFuelCost: {annualFuelCost}</li>
-        <li>spendOnFiveYears: {spendOnFiveYears}</li>
-        <li>feRating: {feRating}</li>
-        <li>ghgRating: {ghgRating}</li>
-        <li>smogRating: {smogRating}</li>
-        <li>ecoScore: {ecoScore}</li>
-      </ul>
-    </div>
-  );
-}
-
-export default DisplayStats;
+  }
+  
+  function DisplayStats({ id, model, carType, priceNew, priceUsed, cylinder, transmissionType, gears, driveSystem, fuel, maxBioFuel, hasStartAndStop, cityFuel, cityCarbon, highwayFuel, highwayCarbon, combinedFuel, combinedCarbon, hasGuzzler, annualFuelCost, spendOnFiveYears, feRating, ghgRating,smogRating, ecoScore  }: Props) {
+      return (
+      <div>
+        <h1>Stats about car</h1>
+        <ul>
+          <li>id: {id}</li>
+          <li>model: {model}</li>
+          <li>carType: {carType}</li>
+          <li>priceNew: {priceNew}</li>
+          <li>priceUsed: {priceUsed}</li>
+          <li>cylinder: {cylinder}</li>
+          <li>transmissionType: {transmissionType}</li>
+          <li>gears: {gears}</li>
+          <li>driveSystem: {driveSystem}</li>
+          <li>fuel: {fuel}</li>
+          <li>maxBioFuel: {maxBioFuel}</li>
+          <li>hasStartAndStop: {hasStartAndStop}</li>
+          <li>cityFuel: {cityFuel}</li>
+          <li>cityCarbon: {cityCarbon}</li>
+          <li>highwayFuel: {highwayFuel}</li>
+          <li>highwayCarbon: {highwayCarbon}</li>
+          <li>combinedFuel: {combinedFuel}</li>
+          <li>combinedCarbon: {combinedCarbon}</li>
+          <li>hasGuzzler: {hasGuzzler}</li>
+          <li>annualFuelCost: {annualFuelCost}</li>
+          <li>spendOnFiveYears: {spendOnFiveYears}</li>
+          <li>feRating: {feRating}</li>
+          <li>ghgRating: {ghgRating}</li>
+          <li>smogRating: {smogRating}</li>
+          <li>ecoScore: {ecoScore}</li>
+        </ul>
+      </div>
+    );
+  }
+  
+  export default DisplayStats;
