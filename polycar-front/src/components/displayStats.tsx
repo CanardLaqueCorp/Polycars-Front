34634fdf29891c62326ed6846d04b764239f1da1
Jules Ginhac <jules.ginhac@etu.univ-lyon1.fr>
2023-09-13 15:04:56 +0200

    table concerning fuel and new librery

--- polycar-front/src/components/displayStats.tsx
+++ polycar-front/src/components/displayStats.tsx
@@ -34,18 +34,27 @@ interface Props {
   smogRating: number;
   ecoScore: number;
   }
-  
+
   function DisplayStats({ id, brand, model, carType, priceNew, priceUsed, cylinder, transmissionType, gears, driveSystem, fuel, maxBioFuel, hasStartAndStop, cityFuel, cityCarbon, highwayFuel, highwayCarbon, combinedFuel, combinedCarbon, hasGuzzler, annualFuelCost, spendOnFiveYears, feRating, ghgRating,smogRating, ecoScore  }: Props) {
-      return (
+    // We check if fuel has more than 10 characters
+    if (fuel.length > 10) {
+      // We cut the string to 10 characters
+      fuel = fuel.substring(0, 9);
+    }
+    // We check if transmissionType has more than 10 characters
+    if (transmissionType.length > 10) {
+        // We cut the string to 10 characters
+        transmissionType = transmissionType.substring(0, 9);
+        }
+    return (
       <div>
         <h1>Stats about car</h1>
         <h3>brand: {brand}</h3>
         <h3>model: {model}</h3>
-
         <div className="card">
             <div className="card-body">
                 <h5 className="card-title">Car Stats</h5>
-                <table className="table"> 
+                <table className="table_card"> 
                     <tbody>
                         <tr>
                             <th scope="row">Car Type</th>
@@ -75,23 +84,58 @@ interface Props {
                             <th scope="row">Drive System</th>
                             <td>{driveSystem}</td>
                         </tr>
+                        <tr>
+                            <th scope="row">Strat And Stop</th>
+                            <td>{hasStartAndStop ? "true" : "false"}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Spend on five years</th>
+                            <td>{spendOnFiveYears}</td>
+                        </tr>
                         </tbody>
                 </table>
             </div>
         </div>
+        <div className="Fuel">
+            <div className="card-body">
+                <h5 className="card-title">Données sur le fuel</h5>
+                <table className="table_Fuel">
+                <tbody>
+                        <tr>
+                            <th scope="row">fuel</th>
+                            <td>{fuel}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">max BioFuel</th>
+                            <td>{maxBioFuel}</td>
+                        </tr>
+                        <tr>
+                            <th scope ="row">city Fuel</th>
+                            <td>{cityFuel}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">highway Fuel</th>
+                            <td>{highwayFuel}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">combined Fuel</th>
+                            <td>{combinedFuel}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">annual Fuel Cost</th>
+                            <td>{annualFuelCost}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <h3>Données sur le carbon</h3>
         <ul>
-          <li>fuel: {fuel}</li>
-          <li>maxBioFuel: {maxBioFuel}</li>
-          <li>hasStartAndStop: {hasStartAndStop}</li>
-          <li>cityFuel: {cityFuel}</li>
           <li>cityCarbon: {cityCarbon}</li>
-          <li>highwayFuel: {highwayFuel}</li>
           <li>highwayCarbon: {highwayCarbon}</li>
-          <li>combinedFuel: {combinedFuel}</li>
           <li>combinedCarbon: {combinedCarbon}</li>
-          <li>hasGuzzler: {hasGuzzler}</li>
-          <li>annualFuelCost: {annualFuelCost}</li>
-          <li>spendOnFiveYears: {spendOnFiveYears}</li>
+          <li>hasGuzzler: 
+            {hasGuzzler ? "true" : "false"} </li>
           <li>feRating: {feRating}</li>
           <li>ghgRating: {ghgRating}</li>
           <li>smogRating: {smogRating}</li>
