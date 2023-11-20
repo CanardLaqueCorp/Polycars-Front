21c75afe1fd9372e93ac0b27fb358f2efa44a234
Claquettes <math@claq.fr>
2023-11-20 08:39:53 +0100

    removed re-declaration in AllCarList

--- polycar-front/src/components/AllCarList.tsx
+++ polycar-front/src/components/AllCarList.tsx
@@ -1,44 +1,7 @@
 import Car from "./Car";
+import { CarData } from "../interface/types";
 import LoadingScreen from "./LoadingScreen";
 import { useEffect, useState } from "react";
-
-
-interface CarData {
-  id: number;
-  brand: string;
-  model: string;
-  carTypeId: number;
-  carType: string;
-  priceNew: number;
-  priceUsed: number;
-  cylinder: number;
-  transmissionTypeId: number;
-  transmissionTypeCode: string;
-  transmissionType: string;
-  transmission: string;
-  gears: number;
-  driveSystemId: number;
-  driveSystemCode: string;
-  driveSystem: string;
-  fuelId: number;
-  fuelCode: string;
-  fuel: string;
-  maxBioFuel: number;
-  hasStartAndStop: boolean;
-  cityFuel: number;
-  cityCarbon: number;
-  highwayFuel: number;
-  highwayCarbon: number;
-  combinedFuel: number;
-  combinedCarbon: number;
-  hasGuzzler: boolean;
-  annualFuelCost: number;
-  spendOnFiveYears: number;
-  feRating: number;
-  ghgRating: number;
-  smogRating: number;
-  ecoScore: number;
-}
 
 function AllCarList() {
   const [filterType, setFilterType] = useState("brand");
