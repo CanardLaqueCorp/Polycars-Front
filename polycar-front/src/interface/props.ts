b448a52040f7f0b26c4833c23794a0e700d5d42f
Claquettes <math@claq.fr>
2023-11-22 12:47:54 +0100

    show number of views roughly in the displaystats

---
+++ polycar-front/src/interface/props.ts
@@ -0,0 +1,54 @@
+<<<<<<< HEAD
+=======
+export interface CarProps {
+  id: number;
+  brand: string;
+  model: string;
+  carTypeId: number;
+  carType: string;
+  priceNew: number;
+  priceUsed: number;
+  cylinder: number;
+  transmissionTypeId: number;
+  transmissionTypeCode: string;
+  transmissionType: string;
+  transmission: string;
+  gears: number;
+  driveSystemId: number;
+  driveSystemCode: string;
+  driveSystem: string;
+  fuelId: number;
+  fuelCode: string;
+  fuel: string;
+  maxBioFuel: number;
+  hasStartAndStop: boolean;
+  cityFuel: number;
+  cityFuelMetric: number;
+  cityCarbon: number;
+  cityCarbonMetric: number;
+  highwayFuel: number;
+  highwayFuelMetric: number;
+  highwayCarbon: number;
+  highwayCarbonMetric: number;
+  combinedFuel: number;
+  combinedFuelMetric: number;
+  combinedCarbon: number;
+  combinedCarbonMetric: number;
+  hasGuzzler: boolean;
+  annualFuelCost: number;
+  annualFuelCostEuro: number;
+  spendOnFiveYears: number;
+  spendOnFiveYearsEuro: number;
+  feRating: number;
+  ghgRating: number;
+  smogRating: number;
+  ecoScore: number;
+  image?: string;
+  views: number;
+<<<<<<< HEAD
+  unit: string;
+=======
+>>>>>>> 8e19f37 (show number of views roughly in the displaystats)
+}
+
+>>>>>>> ddf1315 (show number of views roughly in the displaystats)
