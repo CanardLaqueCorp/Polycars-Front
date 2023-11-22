044e689d6644a8ae4f28ac567c8d4a3fee997bd8
Claquettes <math@claq.fr>
2023-11-22 12:47:54 +0100

    show number of views roughly in the displaystats

--- polycar-front/src/components/AllStatsAboutCar.tsx
+++ polycar-front/src/components/AllStatsAboutCar.tsx
@@ -7,8 +7,6 @@ function AllStatsAboutCar() {
   // Get the car ID from the URL
   const idOfCar: string = window.location.href.split("/")[4];
   const [carData, setCarData] = useState<CarData | null>(null);
-
-
   useEffect(() => {
     fetch(`https://cars.poly-api.fr/public/get/car/${idOfCar}`)
       .then((response) => response.json())
@@ -136,7 +134,6 @@ function AllStatsAboutCar() {
         ecoScore={ecoScore}
         views={views}
         unit={units}
-
       />
     </div>
   );
