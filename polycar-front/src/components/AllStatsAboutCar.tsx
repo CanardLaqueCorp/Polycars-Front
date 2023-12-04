8c6eda26c3f01761f1436a5c8ef3cd611f1bc67c
Claquettes <math@claq.fr>
2023-12-04 09:05:08 +0100

    added metrics in all interfaces

--- polycar-front/src/components/AllStatsAboutCar.tsx
+++ polycar-front/src/components/AllStatsAboutCar.tsx
@@ -93,10 +93,14 @@ function AllStatsAboutCar() {
 =======
         Click to switch units
       </button>
+<<<<<<< HEAD
       
 
 
 >>>>>>> c1b5002 (button to switch units)
+=======
+      Current units: {units}
+>>>>>>> 6e1cad7 (added metrics in all interfaces)
       <DisplayStats
         id={id}
         brand={brand}
